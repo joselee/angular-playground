41b526709ab9d14860974c27f6dfa2441621173b
Lee <joselee@basware.com>
2016-04-21 15:17:05 +0300

    Remove bower. use npm and gulp for everything

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var less = require('gulp-less');
 var ts = require('gulp-typescript');
 var tsLint = require('gulp-tslint');
 var sourcemaps = require('gulp-sourcemaps');
-var mainBowerFiles = require('main-bower-files');
-var filter = require('gulp-filter');
 var listFiles = require('gulp-filesize'); /* .pipe(listFiles()) to see files in stream*/
 var gutil = require('gulp-util');
 var webserver = require('gulp-webserver');
@@ -40,17 +38,33 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('libs', function () {
-	del.sync(['public/build/libs/*.*']);
-	gulp.src(mainBowerFiles())
-		.pipe(filter(['**/*.js', '!bootstrap.js']))
+	del.sync(['public/build/libs/**/*.*']);
+	
+	var libs = {
+		js: [
+			'node_modules/jquery/dist/jquery.min.js',
+			'node_modules/lodash/lodash.min.js',
+			'node_modules/bootstrap/dist/bootstrap.min.js',
+			'node_modules/angular/angular.min.js',
+			'node_modules/angular-bootstrap/ui-bootstrap-tpls.min.js'
+		],
+		styles: [
+			'node_modules/bootstrap/less/bootstrap.less'
+		],
+		fonts: [
+			'node_modules/bootstrap/fonts/*.*'
+		]
+	};
+	
+	gulp.src(libs.js)
 		.pipe(concat('libs.js'))
-		.pipe(gulp.dest('public/build/libs'));
-
-	gulp.src(mainBowerFiles())
-		.pipe(filter(['**/*.less', '**/*.css']))
+		.pipe(gulp.dest('public/build/libs'));		
+	gulp.src(libs.styles)
 		.pipe(less())
 		.pipe(concat('libs.css'))
 		.pipe(gulp.dest('public/build/libs'));
+	gulp.src(libs.fonts)
+		.pipe(gulp.dest('public/build/fonts'));
 });
 
 gulp.task('webserver', function () {
